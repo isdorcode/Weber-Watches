7a1958e84a101d2df33c8d255a518cca636e634c
isdorcode <isdorcode@gmail.com>
2025-01-24 00:09:20 +0200

    run 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Isdor Develops',
   description: 'Travel Application',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return ( 
+  const currentBannerIndex = 0; // Define the default or dynamic value for the current banner index
+
+  return (
     <html lang="en">
       <body>
-       <Navbar />
-       <main className='relative overflow-hidden'>
-         {children}
-       </main>
+        <Navbar currentBannerIndex={currentBannerIndex} /> {/* Pass the prop here */}
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
